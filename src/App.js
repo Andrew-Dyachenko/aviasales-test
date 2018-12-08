54fb73167dcb300029a293b254629901d9787396
Andrew <north.inhale@gmail.com>
2018-12-08 20:45:24 +0300

    Helmet added
    React helmet was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
 import './App.scss'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Helmet>
+					<html lang='en' />
+					<title>Aviasales</title>
+				</Helmet>
 				App
 			</div>
 		)
