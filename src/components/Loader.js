d0746c049f7ae7a9b2f75fa4190500a02fce0ae2
Andrew <north.inhale@gmail.com>
2018-12-15 16:30:42 +0300

    Loader added
    Loader component added

---
+++ src/components/Loader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Loader.css'
+
+const Loader = ({children}) =>
+	<div className="loader">
+		<div className="loader__text">
+			{children}
+		</div>
+	</div>
+
+Loader.defaultProps = {
+	children: 'Loading'
+}
+
+Loader.propTypes = {
+	children: PropTypes.string || PropTypes.element
+}
+
+export default Loader
