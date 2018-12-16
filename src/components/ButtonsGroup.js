ac0b39e633e5aaba03a7d94645873825536976aa
Andrew <north.inhale@gmail.com>
2018-12-17 00:50:54 +0300

    Appearing improvements
    Added some appearing improvements

--- src/components/ButtonsGroup.js
+++ src/components/ButtonsGroup.js
@@ -2,37 +2,47 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './ButtonsGroup.scss'
 
-const ButtonsGroup = ({ buttons, name, title, onChoose }) =>
-	<div className='buttons-group'>
-		{
-			title ?
-				<h4 className='buttons-group__title'>{title}</h4> :
-				null
-		}
-		<div className='buttons-group__container'>
+const ButtonsGroup = ({ buttons, name, title, onChoose }) => {
+	const { length } = buttons
+	return (
+		<div className='buttons-group'>
 			{
-				buttons.map((button, index) => {
-					const { text, defaultChecked } = button
-					return (
-						<div key={index} className='buttons-group__item'>
-							<input
-								className='buttons-group__input'
-								type='radio'
-								name={name}
-								id={`buttons-group_input_${index}`}
-								onChange={onChoose}
-								defaultChecked={defaultChecked} />
-							<label htmlFor={`buttons-group_input_${index}`} className='buttons-group__label'>
-								<span className='buttons-group__text'>
-									{text}
-								</span>
-							</label>
-						</div>
-					)
-				})
+				title ?
+					<h4 className='buttons-group__title'>
+						{title}
+					</h4> :
+					null
+			}
+			{
+				length ?
+					<div className='buttons-group__container'>
+						{
+							buttons.map((button, index) => {
+								const { text, defaultChecked } = button
+								return (
+									<div key={index} className='buttons-group__item'>
+										<input
+											className='buttons-group__input'
+											type='radio'
+											name={name}
+											id={`buttons-group__input_${index}`}
+											onChange={onChoose}
+											defaultChecked={defaultChecked} />
+										<label htmlFor={`buttons-group__input_${index}`} className='buttons-group__label'>
+											<span className='buttons-group__text'>
+												{text}
+											</span>
+										</label>
+									</div>
+								)
+							})
+						}
+					</div> :
+					null
 			}
 		</div>
-	</div>
+	)
+}
 
 ButtonsGroup.defaultProps = {
 	buttons: [],
