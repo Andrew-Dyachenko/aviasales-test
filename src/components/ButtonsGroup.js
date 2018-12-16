52539bf3514555f023a158efb80dd255740ae44a
Andrew <north.inhale@gmail.com>
2018-12-16 23:09:10 +0300

    Extend functional
    Extending buttons group functional

--- src/components/ButtonsGroup.js
+++ src/components/ButtonsGroup.js
@@ -2,31 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './ButtonsGroup.scss'
 
-const ButtonsGroup = ({ buttons, onChoose }) =>
+const ButtonsGroup = ({ buttons, name, title, onChoose }) =>
 	<div className='buttons-group'>
 		{
-			buttons.map((button, index) => {
-				const { text } = button
-				return (
+			title ?
+				<h4 className='buttons-group__title'>{title}</h4> :
+				null
+		}
+		<div className='buttons-group__container'>
+			{
+				buttons.map((button, index) => {
+					const { text, defaultChecked } = button
+					return (
 						<div key={index} className='buttons-group__item'>
-							<input type='radio' id={`buttons-group_input_${index}`}/>
-							<label htmlFor={`buttons-group_input_${index}`} onChange={onChoose}>
-								{text}
+							<input
+								className='buttons-group__input'
+								type='radio'
+								name={name}
+								id={`buttons-group_input_${index}`}
+								onChange={onChoose}
+								defaultChecked={defaultChecked} />
+							<label htmlFor={`buttons-group_input_${index}`} className='buttons-group__label'>
+								<span className='buttons-group__text'>
+									{text}
+								</span>
 							</label>
 						</div>
 					)
-				}
-			)
-		}
+				})
+			}
+		</div>
 	</div>
 
 ButtonsGroup.defaultProps = {
 	buttons: [],
+	name: 'radio-group',
+	title: '',
 	onChoose: f => f
 }
 
 ButtonsGroup.propTypes = {
 	buttons: PropTypes.array,
+	name: PropTypes.string,
+	title: PropTypes.string,
 	onChoose: PropTypes.func
 }
 
