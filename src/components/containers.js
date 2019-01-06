8b355e903458b947554f60c9f79e153b78d643e4
Andrew <north.inhale@gmail.com>
2019-01-06 16:01:15 +0300

    Stops dispatching filter finished
    Finished to build dispatching functionality for stops filter

--- src/components/containers.js
+++ src/components/containers.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { filterTickets } from '../actions'
-import Filter from './UI/Filter'
+/*eslint no-console: 0*/
+import { connect } from 'react-redux'
+import StopList from '../components/UI/StopList'
+import { filterByStops, filterByOnlyStops, filterByAllStops } from '../store/actions'
+import getStops from '../helpers/getStops'
 
-export const FilterTickets = (props, { store }) => (
-	<Filter onNewColor={(title, color) => store.dispatch(filterTickets(title, color))} />
-)
-
-FilterTickets.contextTypes = {
-	store: PropTypes.object
-}
+export const Stops = connect(
+	null,
+	dispatch =>
+		({
+			onStop(event) {
+				const { target } = event
+				const { value } = target
+				const { checked } = target
+				// console.log('value: ', value)
+				dispatch(filterByStops(value, checked))
+			},
+			onOnly(event) {
+				const { target } = event
+				const { dataset } = target
+				const { stops } = dataset
+				console.log('onOnly target dataset index: ', stops)
+				dispatch(filterByOnlyStops(stops))
+			},
+			onAllStops(event) {
+				// console.log('window.store: ', window.store)
+				const { target } = event
+				const { checked } = target
+				const stops = getStops(window.store)
+				// const { dataset } = target
+				// const { array } = dataset
+				console.log('onAllStops stops: ', stops)
+				// dispatch(filterByAllStops())
+				dispatch(filterByAllStops(stops, checked))
+			}
+		})
+)(StopList)
