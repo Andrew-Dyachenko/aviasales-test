758ed49642541563fee977f389e1473ab619413c
Andrew <north.inhale@gmail.com>
2019-01-09 08:04:13 +0300

    Valuta actions
    Valuta actions added

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,49 +1,83 @@
 /*eslint no-console: 0*/
 import C from './constants'
 
-const fetchTicketsStart = () => ({
-		type: C.FETCH_TICKETS_START
-	})
+// Tickets
 
-const fetchTicketsSuccess = tickets => ({
-		type: C.FETCH_TICKETS_SUCCESS,
-		tickets
-	})
+const fetchTicketsStart = () => ({ type: C.FETCH_TICKETS_START })
+const fetchTicketsSuccess = () => ({ type: C.FETCH_TICKETS_SUCCESS })
+const fetchTicketsError = () => ({ type: C.FETCH_TICKETS_ERROR })
 
-const fetchTicketsError = () => ({
-		type: C.FETCH_TICKETS_ERROR
-	})
+const setTickets = tickets => ({
+	type: C.SET_TICKETS,
+	tickets
+})
 
 export const fetchTickets = url => dispatch => {
 	dispatch(fetchTicketsStart())
 	fetch(url)
 		.then(response => response.json())
-		.then(tickets => dispatch(fetchTicketsSuccess(tickets)))
+		.then(tickets => {
+			dispatch(fetchTicketsSuccess())
+			dispatch(setTickets(tickets))
+		})
 		.catch(() => dispatch(fetchTicketsError()))
-	}
+}
 
-export const sortStops = stops => ({
-		type: C.SORT_STOPS,
-		stops
-	})
+// Filters
 
 export const filterByStops = (stops, checked) => ({
-		type: C.FILTER_BY_STOPS,
-		stops,
-		checked
-	})
+	type: C.FILTER_BY_STOPS,
+	stops,
+	checked
+})
 
 export const filterByOnlyStops = stops => ({
-		type: C.FILTER_BY_ONLY_STOPS,
-		stops
-	})
+	type: C.FILTER_BY_ONLY_STOPS,
+	stops
+})
 
 export const filterByDefaultStops = () => ({
-		type: C.FILTER_BY_DEFAULT_STOPS
-	})
+	type: C.FILTER_BY_DEFAULT_STOPS
+})
 
 export const filterByAllStops = (stops, checked) => ({
-		type: C.FILTER_BY_ALL_STOPS,
-		stops,
-		checked
-	})
+	type: C.FILTER_BY_ALL_STOPS,
+	stops,
+	checked
+})
+
+// Valuta
+
+const fetchValutaStart = () => ({ type: C.FETCH_VALUTA_START })
+const fetchValutaSuccess = () => ({ type: C.FETCH_VALUTA_SUCCESS })
+const fetchValutaError = () => ({ type: C.FETCH_VALUTA_ERROR })
+
+const setValutaRates = rates => ({
+	type: C.SET_VALUTA_RATES,
+	rates
+})
+
+export const fetchValuta = url => dispatch => {
+	dispatch(fetchValutaStart())
+	fetch(url)
+		.then(response => response.json())
+		.then(response => {
+			console.log(response)
+			return response
+		})
+		.then(response => {
+			const { rates } = response
+			dispatch(fetchValutaSuccess())
+			dispatch(setValutaRates(rates))
+		})
+		.catch(() => dispatch(fetchValutaError()))
+}
+
+export const setDefaultValuta = () => ({
+	type: C.SET_DEFAULT_VALUTA
+})
+
+export const setBaseValuta = base => ({
+	type: C.SET_BASE_VALUTA,
+	base
+})
