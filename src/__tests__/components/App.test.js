b900480f2cbe5fa797020adaf2ad4dddeb92b603
Andrew <north.inhale@gmail.com>
2019-01-16 09:43:13 +0300

    Reform test after Redux
    Reform App test to FIX it after wrap App of Redux connect and to attend webpack path requirements

---
+++ src/__tests__/components/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../../components/App'
+import { Provider } from 'react-redux'
+import storeFactory from '../../store'
+
+const store = storeFactory()
+
+window.React = React
+window.store = store
+
+
+it('renders without crashing', () => {
+	const div = document.createElement('div')
+	ReactDOM.render(
+		<Provider store={store}>
+			<App />
+		</Provider>, div)
+	ReactDOM.unmountComponentAtNode(div)
+})
