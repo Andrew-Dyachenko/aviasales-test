93b3146c2a784826913cb98c7082c91e1fe81ee9
Andrew <north.inhale@gmail.com>
2019-01-17 19:03:53 +0300

    Replace fetch func
    Replace fetch functions from actions to helpers

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,35 +3,24 @@ import C from './constants'
 
 // Tickets
 
-const fetchTicketsStart = () =>   ({ type: C.FETCH_TICKETS_START })
-const fetchTicketsSuccess = () => ({ type: C.FETCH_TICKETS_SUCCESS })
-const fetchTicketsError = () =>   ({ type: C.FETCH_TICKETS_ERROR })
+export const fetchTicketsStart = () =>   ({ type: C.FETCH_TICKETS_START })
+export const fetchTicketsSuccess = () => ({ type: C.FETCH_TICKETS_SUCCESS })
+export const fetchTicketsError = () =>   ({ type: C.FETCH_TICKETS_ERROR })
 
-const setTickets = tickets => ({
+export const setTickets = tickets => ({
 	type: C.SET_TICKETS,
 	tickets
 })
 
-export const fetchTickets = url => dispatch => {
-	dispatch(fetchTicketsStart())
-	fetch(url)
-		.then(response => response.json())
-		.then(tickets => {
-			dispatch(fetchTicketsSuccess())
-			dispatch(setTickets(tickets))
-		})
-		.catch(() => dispatch(fetchTicketsError()))
-}
-
 // Filters
 
-export const filterByStops = (stops, checked) => ({
+export const filterByStops = (stops = [], checked = false) => ({
 	type: C.FILTER_BY_STOPS,
 	stops,
 	checked
 })
 
-export const filterByOnlyStops = stops => ({
+export const filterByOnlyStops = (stops = []) => ({
 	type: C.FILTER_BY_ONLY_STOPS,
 	stops
 })
@@ -40,7 +29,7 @@ export const filterByDefaultStops = () => ({
 	type: C.FILTER_BY_DEFAULT_STOPS
 })
 
-export const filterByAllStops = (stops, checked) => ({
+export const filterByAllStops = (stops = [], checked = false) => ({
 	type: C.FILTER_BY_ALL_STOPS,
 	stops,
 	checked
@@ -48,30 +37,14 @@ export const filterByAllStops = (stops, checked) => ({
 
 // Currencies
 
-const fetchCurrencyStart = () =>   ({ type: C.FETCH_CURRENCY_START })
-const fetchCurrencySuccess = () => ({ type: C.FETCH_CURRENCY_SUCCESS })
-const fetchCurrencyError = () =>   ({ type: C.FETCH_CURRENCY_ERROR })
+export const fetchCurrencyStart = () =>   ({ type: C.FETCH_CURRENCY_START })
+export const fetchCurrencySuccess = () => ({ type: C.FETCH_CURRENCY_SUCCESS })
+export const fetchCurrencyError = () =>   ({ type: C.FETCH_CURRENCY_ERROR })
 
-const setCurrencyRates = rates => ({
+export const setCurrencyRates = rates => ({
 	type: C.SET_CURRENCY_RATES,
 	rates
 })
-
-export const fetchCurrency = url => dispatch => {
-	dispatch(fetchCurrencyStart())
-	fetch(url)
-		.then(response => response.json())
-		.then(response => {
-			console.log(response)
-			return response
-		})
-		.then(response => {
-			const { rates } = response
-			dispatch(fetchCurrencySuccess())
-			dispatch(setCurrencyRates(rates))
-		})
-		.catch(() => dispatch(fetchCurrencyError()))
-}
 
 export const setDefaultCurrency = () => ({
 	type: C.SET_DEFAULT_CURRENCY
