895a34f0db8a47dbb0f7e501c7fbfda51a144be0
Andrew <north.inhale@gmail.com>
2019-01-25 13:35:27 +0300

    Global tickets freeze
    Global tickets freeze in tests

--- src/__tests__/global.js
+++ src/__tests__/global.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import tickets from '../../public/tickets'
+import deepFreeze from 'deep-freeze'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 global.React = React
 global.Enzyme = Enzyme
-global.tickets = tickets
+global.tickets = deepFreeze(tickets)
