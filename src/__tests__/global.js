0b88b062211ab088c3ab0fda231b37d78e0efb24
Andrew <north.inhale@gmail.com>
2019-01-27 18:36:17 +0300

    global initial state

--- src/__tests__/global.js
+++ src/__tests__/global.js
@@ -3,9 +3,11 @@ import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import tickets from '../../public/tickets'
 import deepFreeze from 'deep-freeze'
+import initialState from '../data/initialState'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 global.React = React
 global.Enzyme = Enzyme
 global.tickets = deepFreeze(tickets)
+global.initialState = deepFreeze(initialState)
