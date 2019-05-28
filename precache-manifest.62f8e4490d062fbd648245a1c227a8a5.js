759c7d809147bfc80e9a6b6103ffe9be90f22bd0
Andrew <north.inhale@gmail.com>
2019-05-28 15:44:59 +0300

    Initial build subtree commit

---
+++ precache-manifest.62f8e4490d062fbd648245a1c227a8a5.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = [
+  {
+    "revision": "da7f5fc8bd49aabae2ad7b9844c4db80",
+    "url": "./static/media/OpenSans-SemiBoldItalic.da7f5fc8.woff"
+  },
+  {
+    "revision": "788dddf836ed34cee7f4",
+    "url": "./static/css/main.8cc78a43.chunk.css"
+  },
+  {
+    "revision": "6ac97ae332e9df9e7527",
+    "url": "./static/js/1.6ac97ae3.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "213d290e5fdacc0b14bd6e5b1d59418d",
+    "url": "./static/media/turkish-airlines-logo.213d290e.svg"
+  },
+  {
+    "revision": "9a2b9fa2e2a97dcaf01198c9b651046d",
+    "url": "./static/media/logo.9a2b9fa2.png"
+  },
+  {
+    "revision": "ce1afdb370d7aeec7f89f0ec340ad7b4",
+    "url": "./static/media/OpenSans-LightItalic.ce1afdb3.eot"
+  },
+  {
+    "revision": "1707086874e1c0a9fce7f81abdd51a64",
+    "url": "./static/media/OpenSans-LightItalic.17070868.woff2"
+  },
+  {
+    "revision": "2f38f6c7f86e3c19ed56beb84d127c21",
+    "url": "./static/media/OpenSans-LightItalic.2f38f6c7.woff"
+  },
+  {
+    "revision": "0de79a77142ed7150dd2d1f8248a7e48",
+    "url": "./static/media/OpenSans-LightItalic.0de79a77.ttf"
+  },
+  {
+    "revision": "0936169106eb03190ee2df8570d7abd7",
+    "url": "./static/media/OpenSans-LightItalic.09361691.svg"
+  },
+  {
+    "revision": "52904e0e816c2cb9d7dadf39ff3d3543",
+    "url": "./static/media/OpenSans-Light.52904e0e.eot"
+  },
+  {
+    "revision": "43ef6231263e2626b02ef41dc37e60a3",
+    "url": "./static/media/OpenSans-Light.43ef6231.woff2"
+  },
+  {
+    "revision": "0ddda46199eb58c9b7ade03715eabe82",
+    "url": "./static/media/OpenSans-Light.0ddda461.woff"
+  },
+  {
+    "revision": "56baf4dc8aebdc091c2cb4aa83fc4842",
+    "url": "./static/media/OpenSans-Light.56baf4dc.ttf"
+  },
+  {
+    "revision": "a2e5ac0c97e710c78383e76b421dec19",
+    "url": "./static/media/OpenSans-Light.a2e5ac0c.svg"
+  },
+  {
+    "revision": "4c8b54a45dce8eda1856bdecc771a91f",
+    "url": "./static/media/OpenSans-Italic.4c8b54a4.eot"
+  },
+  {
+    "revision": "28aa55c5f1e80919af822d255947e8b5",
+    "url": "./static/media/OpenSans-Italic.28aa55c5.woff2"
+  },
+  {
+    "revision": "43c0acc64bba72fa84194afff7ac28eb",
+    "url": "./static/media/OpenSans-Italic.43c0acc6.woff"
+  },
+  {
+    "revision": "83a5eb93e36078a69e0fefe616e1e208",
+    "url": "./static/media/OpenSans-Italic.83a5eb93.ttf"
+  },
+  {
+    "revision": "7d198e7b7cfa456cacc292ef24970064",
+    "url": "./static/media/OpenSans-Italic.7d198e7b.svg"
+  },
+  {
+    "revision": "edaf0d2c447b7a58cbbe740457dcf32a",
+    "url": "./static/media/OpenSans-Regular.edaf0d2c.eot"
+  },
+  {
+    "revision": "4089b4500c8dafe98adb7f55a535c354",
+    "url": "./static/media/OpenSans-Regular.4089b450.woff2"
+  },
+  {
+    "revision": "e0d4792d128bba2585f6d58215c74e60",
+    "url": "./static/media/OpenSans-Regular.e0d4792d.woff"
+  },
+  {
+    "revision": "bc320dbc9b69b1fd723df997fc316abb",
+    "url": "./static/media/OpenSans-Regular.bc320dbc.ttf"
+  },
+  {
+    "revision": "3b84f65ed57480a012eec335fdd9ede0",
+    "url": "./static/media/OpenSans-Regular.3b84f65e.svg"
+  },
+  {
+    "revision": "3b7c5d8b1970e709629916476de18ad9",
+    "url": "./static/media/OpenSans-SemiBoldItalic.3b7c5d8b.eot"
+  },
+  {
+    "revision": "f865b2b96e4e56d8800a92e3c38cbbe2",
+    "url": "./static/media/OpenSans-SemiBoldItalic.f865b2b9.woff2"
+  },
+  {
+    "revision": "788dddf836ed34cee7f4",
+    "url": "./static/js/main.788dddf8.chunk.js"
+  },
+  {
+    "revision": "395f395a1e3547fd4905f16e9a2a3564",
+    "url": "./static/media/OpenSans-SemiBoldItalic.395f395a.ttf"
+  },
+  {
+    "revision": "0831229f77a3891069044df7d541f560",
+    "url": "./static/media/OpenSans-SemiBoldItalic.0831229f.svg"
+  },
+  {
+    "revision": "a0cc1c8c1721407ebe7ab4867ee428e5",
+    "url": "./static/media/OpenSans-SemiBold.a0cc1c8c.eot"
+  },
+  {
+    "revision": "c677693bd9501d11c95416fba8e9ebe8",
+    "url": "./static/media/OpenSans-SemiBold.c677693b.woff2"
+  },
+  {
+    "revision": "368f691ed473c38fd69f8c9f4a833f15",
+    "url": "./static/media/OpenSans-SemiBold.368f691e.woff"
+  },
+  {
+    "revision": "824b4a796f966bac46af756d7b1fb9c6",
+    "url": "./static/media/OpenSans-SemiBold.824b4a79.ttf"
+  },
+  {
+    "revision": "1966da586a4ce6aad36af86aa733995d",
+    "url": "./static/media/OpenSans-SemiBold.1966da58.svg"
+  },
+  {
+    "revision": "d5e90efee5461c4f86129d0e2594cd2e",
+    "url": "./static/media/OpenSans-BoldItalic.d5e90efe.eot"
+  },
+  {
+    "revision": "1a9e41dd2e1b851cbce340902b3133be",
+    "url": "./static/media/OpenSans-BoldItalic.1a9e41dd.woff2"
+  },
+  {
+    "revision": "a576b4ba2fe4cbb46e435e1b525df38e",
+    "url": "./static/media/OpenSans-BoldItalic.a576b4ba.woff"
+  },
+  {
+    "revision": "cd1b3e797d689b01322e57059ced5e08",
+    "url": "./static/media/OpenSans-BoldItalic.cd1b3e79.ttf"
+  },
+  {
+    "revision": "4a0d7add33609e07d9124553e2fe0cee",
+    "url": "./static/media/OpenSans-BoldItalic.4a0d7add.svg"
+  },
+  {
+    "revision": "5c1bcadfb94d5c0db733965d7a0c989c",
+    "url": "./static/media/OpenSans-Bold.5c1bcadf.eot"
+  },
+  {
+    "revision": "1cc5a37c34b538977a4b383f030da4a9",
+    "url": "./static/media/OpenSans-Bold.1cc5a37c.woff2"
+  },
+  {
+    "revision": "dad6420b2aee991d2da37eb12ed40f7a",
+    "url": "./static/media/OpenSans-Bold.dad6420b.woff"
+  },
+  {
+    "revision": "eff1a4f126b18054ae8b440d6d03e388",
+    "url": "./static/media/OpenSans-Bold.eff1a4f1.ttf"
+  },
+  {
+    "revision": "b47e1e7411a21a8b5fdb2d3173eef16b",
+    "url": "./static/media/OpenSans-Bold.b47e1e74.svg"
+  },
+  {
+    "revision": "961b2ad860aacc30b6d6278cf53d2f7f",
+    "url": "./static/media/OpenSans-ExtraBoldItalic.961b2ad8.eot"
+  },
+  {
+    "revision": "901d3308c5a4f6db3b0fd016627fdac7",
+    "url": "./static/media/OpenSans-ExtraBoldItalic.901d3308.woff2"
+  },
+  {
+    "revision": "59d51fe271299a39eb7d4c96a679cedf",
+    "url": "./static/media/OpenSans-ExtraBoldItalic.59d51fe2.woff"
+  },
+  {
+    "revision": "853baca934a8faecb3fbe923c1ee2233",
+    "url": "./static/media/OpenSans-ExtraBoldItalic.853baca9.ttf"
+  },
+  {
+    "revision": "217e2eedae6906c93d4cdf44d90dbe8f",
+    "url": "./static/media/OpenSans-ExtraBoldItalic.217e2eed.svg"
+  },
+  {
+    "revision": "306dd9271ea84172bc20315961d1550d",
+    "url": "./static/media/OpenSans-ExtraBold.306dd927.eot"
+  },
+  {
+    "revision": "bd47ff8757169984bf385b9c6c8181f5",
+    "url": "./static/media/OpenSans-ExtraBold.bd47ff87.woff2"
+  },
+  {
+    "revision": "0954e26e0dfcf2016cc770ca0f351612",
+    "url": "./static/media/OpenSans-ExtraBold.0954e26e.woff"
+  },
+  {
+    "revision": "c75d9f83bfa891dc8c4cb380742999cc",
+    "url": "./static/media/OpenSans-ExtraBold.c75d9f83.ttf"
+  },
+  {
+    "revision": "afdbcdb22db69145bb434d2a06346259",
+    "url": "./static/media/OpenSans-ExtraBold.afdbcdb2.svg"
+  },
+  {
+    "revision": "6ff3c93dd96f7f99faf5fce9e8c396ed",
+    "url": "./index.html"
+  }
+];
