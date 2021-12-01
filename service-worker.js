22b9b0ed08ec46ee459479eef83e4afefcea4b33
Andrew-Dyachenko <north.inhale@gmail.com>
2021-12-01 13:23:53 +0300

    feat(gh-pages): update to 1.1.4
    Update gh-pages, Github demo page recources to the latest version from 1.1.2 to 1.1.4

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.62f8e4490d062fbd648245a1c227a8a5.js"
+  "./precache-manifest.661b829568a70a35c98b5f68e88740a6.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
